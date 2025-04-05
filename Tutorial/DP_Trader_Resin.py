4b4b4cfe6d2f14d9428804093bd5f6f71897877d
Samuel Gebhardt <sgebhardt@student.ethz.ch>
2025-04-05 14:24:37 +0200

    add DP_Trader_Resin

---
+++ Tutorial/DP_Trader_Resin.py
@@ -0,0 +1,465 @@
+import json
+import jsonpickle
+from typing import Any, List
+
+from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
+
+RESIN_TRADES = "shhhhhhhhhhhbshhhhshbhhhhhhhhhhhhhhhbshhhhhbhhhhhhhhhhhhhhhhhhhhhbhhhhhhhshhhhhhhhhhhhhhbhhhhhhhhhhhhhhhshhhhhhhhhhhhhhhshhhhhhhhhshhhhhhbhhhhhhhhhhhhhhhhbshshhhhbhhhhhhhhhhhhhhhhhhhhhhhhhhhhhbhhhhshhhsbhhhhhhhhhbhhhshhshhhhbhhhbhhhhhhhhshhhhhhssbhhshhbhhbhhshhhhbhhbhhhhhhhhhhhhhhhbhhhshhhhhhhshhhhhhhhhhhhhhhhhhhbhhhhhhhhhhhhhhhhhhhhhhhhhhhhshhhhhhhshhhhhhhhhsbhhhhhhhhhhbshhhhhshhhhhhhhhhhhhhbhhhhhhhbhshhhhhhhhhhhhhbhhshhhhhhhhhhhhhhhhshbhhhhhhbhshhhbhhhhhhhhhhhhhhhhbshhhhhhhhhhbhbhhhhhhhhhhhhhhhhhhhhshshhhhhhhhhhhhhhshbhhhhshhhhhbhhhhhhhhhhhhhhhhbhhhhhhhhhshhhhshhhshhhhhhbbhhhhhshhhhhhhhshhhhhhhhbhhhshhhhhhhhhhbhhhhhhhhhbhhhhhhhhhhhhhhhhhhhhbhhhhhhhhhhbshbhhhhhhhhbhhhhhhhhhhhhhhbhhhhhshhshhhhhbhhhhhhhhshhsbhsshhhhshhhhhhhhhbhhhhhshhhhhhhhsbhhhhhshhhhbhhhhhhhhhhhhhhhhhsbshhhhhbshhhbhhhhhhhhhhhhhhhhhhhhhhhhhsbhhshhhhhhbhbhhhhhhhhbhhhhhbhhhbhhhhhhshbhshhhhhhshhhhhhhhhhhhbshhhhhhhhhhhhhhhhhhhhhhhhshhhhhhhhhbhhhbhbhhhshhhhbhbhhsbhshhshhhshbhhhhhhhshhhhbhhhhhshhhhhhhhhhhhbhhhhhhshhhhhhhhhhhhhhhhhhshhhhhbhhhhhhshhhhhhhhhhhhhhhhhhhhhhhhhhshhhhhhhhhhshhhhbhhbbshhhhhhhhhhhbhhhhhbhhhhhhhhhbhsshhhhhhshhhbbhbhhhhhhhbhhhhhhhhshhhhhhhshbhhhhhshhhhhhbhhhhhhhhhhhhhsshhhhhhhhhhhhhhhhhhhhhhhhbhhhhhsshshhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhsbhhhhhhbhbhhhhshhhhbbhhshhhhhhhhhhhhhbhhhhhhshhhhhhhhhhhhhhhhhhbhshhhhhhhhhhbhhhhhhhhhhhhhhbhshhhhhhhhhhhshhhshbhhhhhhhhhhbhhshhhhshhhhhhbhbhhhhbhbhhhhhhhhhshbhhhhhhshhhhhhhhhhhhhhhhhhhhshhshbhhhhhhhhhshhhhhhshhhhhhbhsbhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhshshhhhshhhhbhbshhbhhhhhhhshhhbhhhhhhhhhhhhhhbhhhhhhhhhhhhshhhshhbhhhhhhhhshhhhhhbhhshhhhhhhsbbhhhhhhhhhhhhbhhhhhbhhshshhhhhhhbhhhhhhhhhhhhhhbhhhshhbhbhhhhhbhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhshhhhhhhhhhhhhhhhhhshhhhhhshhssbhhshshhhhhbhhhhhhhbhhhhhhhhhhhhhhhhhhhhhhhhshhhhhhhhhhhhhhhbhhbhhhhhhhhhhhhhhhhhhhhhbhbhhhhhhhhhhhhhhhsshhhhshhhhhhhhhhhhhhhhhhhhhhsshhhhhbhhhhshhhbhhhhhhhhhhhhhhhshhhbhhhhhhhhhhbhhhhhhhhhhhsbshhhhhbhhhhhbbhhbshhhhhhshbhbhhhbhhhhhhhhhshhhhhshbshhhhhhhhhhhhhshhhhhhhhhhbhhhshhhhhshhhh"
+
+class Product:
+    RAINFOREST_RESIN = "RAINFOREST_RESIN"
+
+
+
+PARAMS = {
+    Product.RAINFOREST_RESIN: {
+        "fair_value": 10000,
+        "take_width": 1,
+        "clear_width": 0.5,
+        "volume_limit": 0,
+    }
+}
+
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+        self.max_log_length = 3750
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]], conversions: int, trader_data: str) -> None:
+        base_length = len(
+            self.to_json(
+                [
+                    self.compress_state(state, ""),
+                    self.compress_orders(orders),
+                    conversions,
+                    "",
+                    "",
+                ]
+            )
+        )
+
+        # We truncate state.traderData, trader_data, and self.logs to the same max. length to fit the log limit
+        max_item_length = (self.max_log_length - base_length) // 3
+
+        print(
+            self.to_json(
+                [
+                    self.compress_state(state, self.truncate(state.traderData, max_item_length)),
+                    self.compress_orders(orders),
+                    conversions,
+                    self.truncate(trader_data, max_item_length),
+                    self.truncate(self.logs, max_item_length),
+                ]
+            )
+        )
+
+        self.logs = ""
+
+    def compress_state(self, state: TradingState, trader_data: str) -> list[Any]:
+        return [
+            state.timestamp,
+            trader_data,
+            self.compress_listings(state.listings),
+            self.compress_order_depths(state.order_depths),
+            self.compress_trades(state.own_trades),
+            self.compress_trades(state.market_trades),
+            state.position,
+            self.compress_observations(state.observations),
+        ]
+
+    def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
+        compressed = []
+        for listing in listings.values():
+            compressed.append([listing.symbol, listing.product, listing.denomination])
+
+        return compressed
+
+    def compress_order_depths(self, order_depths: dict[Symbol, OrderDepth]) -> dict[Symbol, list[Any]]:
+        compressed = {}
+        for symbol, order_depth in order_depths.items():
+            compressed[symbol] = [order_depth.buy_orders, order_depth.sell_orders]
+
+        return compressed
+
+    def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
+        compressed = []
+        for arr in trades.values():
+            for trade in arr:
+                compressed.append(
+                    [
+                        trade.symbol,
+                        trade.price,
+                        trade.quantity,
+                        trade.buyer,
+                        trade.seller,
+                        trade.timestamp,
+                    ]
+                )
+
+        return compressed
+
+    def compress_observations(self, observations: Observation) -> list[Any]:
+        conversion_observations = {}
+        for product, observation in observations.conversionObservations.items():
+            conversion_observations[product] = [
+                observation.bidPrice,
+                observation.askPrice,
+                observation.transportFees,
+                observation.exportTariff,
+                observation.importTariff,
+                observation.sugarPrice,
+                observation.sunlightIndex,
+            ]
+
+        return [observations.plainValueObservations, conversion_observations]
+
+    def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
+        compressed = []
+        for arr in orders.values():
+            for order in arr:
+                compressed.append([order.symbol, order.price, order.quantity])
+
+        return compressed
+
+    def to_json(self, value: Any) -> str:
+        return json.dumps(value, cls=ProsperityEncoder, separators=(",", ":"))
+
+    def truncate(self, value: str, max_length: int) -> str:
+        if len(value) <= max_length:
+            return value
+
+        return value[: max_length - 3] + "..."
+
+
+logger = Logger()
+
+
+class Trader:
+
+    def __init__(self, params=None):
+        if params is None:
+            params = PARAMS
+        self.params = params
+
+        self.LIMIT = {
+            Product.RAINFOREST_RESIN: 50,
+        }
+
+    def market_make(
+            self,
+            product: str,
+            orders: List[Order],
+            bid: int,
+            ask: int,
+            position: int,
+            buy_order_volume: int,
+            sell_order_volume: int,
+    ) -> (int, int):
+        buy_quantity = self.LIMIT[product] - (position + buy_order_volume)
+        if buy_quantity > 0:
+            orders.append(Order(product, round(bid), buy_quantity))  # Buy order
+
+        sell_quantity = self.LIMIT[product] + (position - sell_order_volume)
+        if sell_quantity > 0:
+            orders.append(Order(product, round(ask), -sell_quantity))  # Sell order
+        return buy_order_volume, sell_order_volume
+
+    def take_best_orders(
+            self,
+            product: str,
+            fair_value: int,
+            take_width: float,
+            orders: List[Order],
+            order_depth: OrderDepth,
+            position: int,
+            buy_order_volume: int,
+            sell_order_volume: int,
+            prevent_adverse: bool = False,
+            adverse_volume: int = 0,
+    ) -> (int, int):
+        position_limit = self.LIMIT[product]
+        if len(order_depth.sell_orders) != 0:
+            best_ask = min(order_depth.sell_orders.keys())
+            best_ask_amount = -1 * order_depth.sell_orders[best_ask]
+
+            if best_ask <= fair_value - take_width:
+                quantity = min(
+                    best_ask_amount, position_limit - position
+                )  # max amt to buy
+                if quantity > 0:
+                    orders.append(Order(product, best_ask, quantity))
+                    buy_order_volume += quantity
+                    order_depth.sell_orders[best_ask] += quantity
+                    if order_depth.sell_orders[best_ask] == 0:
+                        del order_depth.sell_orders[best_ask]
+
+        if len(order_depth.buy_orders) != 0:
+            best_bid = max(order_depth.buy_orders.keys())
+            best_bid_amount = order_depth.buy_orders[best_bid]
+            if best_bid >= fair_value + take_width:
+                quantity = min(
+                    best_bid_amount, position_limit + position
+                )  # should be the max we can sell
+                if quantity > 0:
+                    orders.append(Order(product, best_bid, -1 * quantity))
+                    sell_order_volume += quantity
+                    order_depth.buy_orders[best_bid] -= quantity
+                    if order_depth.buy_orders[best_bid] == 0:
+                        del order_depth.buy_orders[best_bid]
+        return buy_order_volume, sell_order_volume
+
+    def take_best_orders_with_adverse(
+            self,
+            product: str,
+            fair_value: int,
+            take_width: float,
+            orders: List[Order],
+            order_depth: OrderDepth,
+            position: int,
+            buy_order_volume: int,
+            sell_order_volume: int,
+            adverse_volume: int,
+    ) -> (int, int):
+
+        position_limit = self.LIMIT[product]
+        if len(order_depth.sell_orders) != 0:
+            best_ask = min(order_depth.sell_orders.keys())
+            best_ask_amount = -1 * order_depth.sell_orders[best_ask]
+            if abs(best_ask_amount) <= adverse_volume:
+                if best_ask <= fair_value - take_width:
+                    quantity = min(
+                        best_ask_amount, position_limit - position
+                    )  # max amt to buy
+                    if quantity > 0:
+                        orders.append(Order(product, best_ask, quantity))
+                        buy_order_volume += quantity
+                        order_depth.sell_orders[best_ask] += quantity
+                        if order_depth.sell_orders[best_ask] == 0:
+                            del order_depth.sell_orders[best_ask]
+
+        if len(order_depth.buy_orders) != 0:
+            best_bid = max(order_depth.buy_orders.keys())
+            best_bid_amount = order_depth.buy_orders[best_bid]
+            if abs(best_bid_amount) <= adverse_volume:
+                if best_bid >= fair_value + take_width:
+                    quantity = min(
+                        best_bid_amount, position_limit + position
+                    )  # should be the max we can sell
+                    if quantity > 0:
+                        orders.append(Order(product, best_bid, -1 * quantity))
+                        sell_order_volume += quantity
+                        order_depth.buy_orders[best_bid] -= quantity
+                        if order_depth.buy_orders[best_bid] == 0:
+                            del order_depth.buy_orders[best_bid]
+
+        return buy_order_volume, sell_order_volume
+
+    def make_rainforest_resin_orders(
+            self,
+            order_depth: OrderDepth,
+            fair_value: int,
+            position: int,
+            buy_order_volume: int,
+            sell_order_volume: int,
+            volume_limit: int,
+    ) -> (List[Order], int, int):
+        orders: List[Order] = []
+        baaf = min(
+            [
+                price
+                for price in order_depth.sell_orders.keys()
+                if price > fair_value + 1
+            ]
+        )
+        bbbf = max(
+            [price for price in order_depth.buy_orders.keys() if price < fair_value - 1]
+        )
+
+        if baaf <= fair_value + 2:
+            if position <= volume_limit:
+                baaf = fair_value + 3  # still want edge 2 if position is not a concern
+
+        if bbbf >= fair_value - 2:
+            if position >= -volume_limit:
+                bbbf = fair_value - 3  # still want edge 2 if position is not a concern
+
+        buy_order_volume, sell_order_volume = self.market_make(
+            Product.RAINFOREST_RESIN,
+            orders,
+            bbbf + 1,
+            baaf - 1,
+            position,
+            buy_order_volume,
+            sell_order_volume,
+        )
+        return orders, buy_order_volume, sell_order_volume
+
+    def rainforest_resin_order(
+            self, order_depth: OrderDepth, position: int, timestamp: int
+    ) -> List[Order]:
+        iteration = int(timestamp / 100)
+        signal = RESIN_TRADES[iteration]
+        best_bid = max(order_depth.buy_orders.keys())
+        worst_bid = min(order_depth.buy_orders.keys())
+        best_ask = min(order_depth.sell_orders.keys())
+        worst_ask = max(order_depth.sell_orders.keys())
+        mid_price = (best_bid + best_ask) / 2
+
+        if signal == "s":
+            target_volume = self.LIMIT[Product.RAINFOREST_RESIN] - position
+            if target_volume > 0:
+                return [Order(Product.RAINFOREST_RESIN, worst_ask, target_volume)]
+            else:
+                return []
+
+        elif signal == "b":
+            target_volume = -self.LIMIT[Product.RAINFOREST_RESIN] - position
+            if target_volume < 0:
+                return [Order(Product.RAINFOREST_RESIN, worst_bid, target_volume)]
+            else:
+                return []
+
+        else:
+            return []
+
+    def clear_position_order(
+            self,
+            product: str,
+            fair_value: float,
+            width: int,
+            orders: List[Order],
+            order_depth: OrderDepth,
+            position: int,
+            buy_order_volume: int,
+            sell_order_volume: int,
+    ) -> List[Order]:
+        position_after_take = position + buy_order_volume - sell_order_volume
+        fair_for_bid = round(fair_value - width)
+        fair_for_ask = round(fair_value + width)
+
+        buy_quantity = self.LIMIT[product] - (position + buy_order_volume)
+        sell_quantity = self.LIMIT[product] + (position - sell_order_volume)
+
+        if position_after_take > 0:
+            # Aggregate volume from all buy orders with price greater than fair_for_ask
+            clear_quantity = sum(
+                volume
+                for price, volume in order_depth.buy_orders.items()
+                if price >= fair_for_ask
+            )
+            clear_quantity = min(clear_quantity, position_after_take)
+            sent_quantity = min(sell_quantity, clear_quantity)
+            if sent_quantity > 0:
+                orders.append(Order(product, fair_for_ask, -abs(sent_quantity)))
+                sell_order_volume += abs(sent_quantity)
+
+        if position_after_take < 0:
+            # Aggregate volume from all sell orders with price lower than fair_for_bid
+            clear_quantity = sum(
+                abs(volume)
+                for price, volume in order_depth.sell_orders.items()
+                if price <= fair_for_bid
+            )
+            clear_quantity = min(clear_quantity, abs(position_after_take))
+            sent_quantity = min(buy_quantity, clear_quantity)
+            if sent_quantity > 0:
+                orders.append(Order(product, fair_for_bid, abs(sent_quantity)))
+                buy_order_volume += abs(sent_quantity)
+
+        return buy_order_volume, sell_order_volume
+
+    def take_orders(
+            self,
+            product: str,
+            order_depth: OrderDepth,
+            fair_value: float,
+            take_width: float,
+            position: int,
+            prevent_adverse: bool = False,
+            adverse_volume: int = 0,
+    ) -> (List[Order], int, int):
+        orders: List[Order] = []
+        buy_order_volume = 0
+        sell_order_volume = 0
+
+        if prevent_adverse:
+            buy_order_volume, sell_order_volume = self.take_best_orders_with_adverse(
+                product,
+                fair_value,
+                take_width,
+                orders,
+                order_depth,
+                position,
+                buy_order_volume,
+                sell_order_volume,
+                adverse_volume,
+            )
+        else:
+            buy_order_volume, sell_order_volume = self.take_best_orders(
+                product,
+                fair_value,
+                take_width,
+                orders,
+                order_depth,
+                position,
+                buy_order_volume,
+                sell_order_volume,
+            )
+        return orders, buy_order_volume, sell_order_volume
+
+    def clear_orders(
+            self,
+            product: str,
+            order_depth: OrderDepth,
+            fair_value: float,
+            clear_width: int,
+            position: int,
+            buy_order_volume: int,
+            sell_order_volume: int,
+    ) -> (List[Order], int, int):
+        orders: List[Order] = []
+        buy_order_volume, sell_order_volume = self.clear_position_order(
+            product,
+            fair_value,
+            clear_width,
+            orders,
+            order_depth,
+            position,
+            buy_order_volume,
+            sell_order_volume,
+        )
+        return orders, buy_order_volume, sell_order_volume
+
+
+    def run(self, state: TradingState) -> tuple[dict[Symbol, list[Order]], int, str]:
+        result = {}
+        timestamp = state.timestamp
+        conversions = 0
+        trader_data = ""
+
+        if Product.RAINFOREST_RESIN in self.params and Product.RAINFOREST_RESIN in state.order_depths:
+            rainforest_resin_position = (
+                state.position[Product.RAINFOREST_RESIN]
+                if Product.RAINFOREST_RESIN in state.position
+                else 0
+            )
+
+            rainforest_resin_order = self.rainforest_resin_order(
+                state.order_depths[Product.RAINFOREST_RESIN], rainforest_resin_position, timestamp
+            )
+            if rainforest_resin_order is not None and len(rainforest_resin_order) > 0:
+                result[Product.RAINFOREST_RESIN] = rainforest_resin_order
+
+        # --------------------------return value ajustments----------------------------------------------------
+        # this will become important later in the game
+        conversions = 1
+
+        logger.flush(state, result, conversions, state.traderData)
+        # backup the recent version of the logger object
+        # trader_data_object["logger"] = logger
+        # traderData = jsonpickle.encode(trader_data_object)
+
+        return result, conversions, str(state.traderData)
